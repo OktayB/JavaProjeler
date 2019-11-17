356e17d5af27006176bde5097a7f90ab1b4549ca
OktayB <oktayuysal1905@gmail.com>
2019-11-17 20:26:42 +0100

    2. versiyon

--- TersIslem.java
+++ TersIslem.java
@@ -1,23 +1,31 @@
+  
 import java.util.Scanner;
 
 public class TersIslem {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void kelime () {
 		
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.println("Kelime Giriniz: ");
-		
 		String kelime = scanner.nextLine();
-		
-		//System.out.println(kelime.toUpperCase());
 		
 		char[] a = kelime.toCharArray(); 
 		  
         for (int i = a.length-1; i>=0; i--) 
-            System.out.print(a[i]);
+                 
+		System.out.print(a[i]);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+					
+		kelime();
 
+              
 	}
 
 }

8f1c93a075d82d39a1f59f26766c538baa7beeaf
OktayB <oktayuysal1905@gmail.com>
2019-11-17 20:38:25 +0100

    3. versiyon

--- TersIslem.java
+++ TersIslem.java
@@ -1,31 +1,19 @@
-  
-import java.util.Scanner;
+ public class TersIslem {
 
-public class TersIslem {
-
-	static void kelime () {
+	static void word(String s) {
+				
+		System.out.println(s);
+				
+		char[] a = s.toCharArray(); 
+		for (int i = a.length-1; i>=0; i--) 
+        System.out.print(a[i]);
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Kelime Giriniz: ");
-		String kelime = scanner.nextLine();
-		
-		char[] a = kelime.toCharArray(); 
-		  
-        for (int i = a.length-1; i>=0; i--) 
-                 
-		System.out.print(a[i]);
-		
-	}
-	
-	
+		}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-					
-		kelime();
-
-              
-	}
+							
+		word("HelloWorld");
+    
+		}
 
 }
